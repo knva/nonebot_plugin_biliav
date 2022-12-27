8d022d9ed323f27298dc44de99d549aee7398cb4
心碎男妈妈 <txt_ch@163.com>
2022-12-27 18:44:09 +0800

    Update __init__.py

--- nonebot_plugin_biliav/__init__.py
+++ nonebot_plugin_biliav/__init__.py
@@ -5,7 +5,7 @@ from .config import Config
 from nonebot.typing import T_State
 from pathlib import Path
 from nonebot.adapters.onebot.v11 import Bot, Event,Message
-from nonebot.params import T_State,State
+from nonebot.params import T_State
 
 global_config = get_driver().config
 config = Config(**global_config.dict())
@@ -21,7 +21,7 @@ import re
 #     pass
 biliav = on_regex("av(\d{1,12})|BV(1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2})")
 @biliav.handle()
-async def handle(bot:Bot,event:Event,state:T_State=State()):
+async def handle(bot:Bot,event:Event,state:T_State):
     avcode = re.search('av(\d{1,12})|BV(1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2})', str(event.get_message()))
     if avcode==None:
         return
