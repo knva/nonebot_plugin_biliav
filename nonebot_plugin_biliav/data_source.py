8a40d973e72ff086ca3cfe8321e81a12bc2a64a5
RiotGamesU <67886642+RiotGamesU@users.noreply.github.com>
2023-01-17 22:08:04 +0800

    Update data_source.py

--- nonebot_plugin_biliav/data_source.py
+++ nonebot_plugin_biliav/data_source.py
@@ -1,29 +1,48 @@
 import httpx
 import json
+import re
 import nonebot
-
 from nonebot.adapters.onebot.v11 import MessageSegment, Message, Bot, Event
 
 url = 'https://api.bilibili.com/x/web-interface/view'
 
 global_config = nonebot.get_driver().config
 config = global_config.dict()
-b_comments = config.get('b_comments',"True")
-if b_comments == "False":
-    b_comments = False
-else:
-    b_comments = True
+b_comments = config.get('b_comments', False)
+b_b23tv = config.get('b_b23tv', True)
 
+if type(b_comments) != bool and b_comments == "True":
+    b_comments = True
+else:
+    b_comments = False
+
+if type(b_b23tv) != bool and b_b23tv == "False":
+    b_b23tv = False
+else:
+    b_b23tv = True
 
 import math
+
+
+async def b23tv2bv(b23tv: str) -> str:
+    async with httpx.AsyncClient() as client:
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        r = await client.get(b23tv, headers=headers)
+    return re.findall("[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}", str(r.next_request.url))[0]
+
+
 def bv2av(Bv):
     # 1.去除Bv号前的"Bv"字符
     BvNo1 = Bv[2:]
     keys = {
-        '1':'13', '2':'12', '3':'46', '4':'31', '5':'43', '6':'18', '7':'40', '8':'28', '9':'5',
-        'A':'54', 'B':'20', 'C':'15', 'D':'8', 'E':'39', 'F':'57', 'G':'45', 'H':'36', 'J':'38', 'K':'51', 'L':'42', 'M':'49', 'N':'52', 'P':'53', 'Q':'7', 'R':'4', 'S':'9', 'T':'50', 'U':'10', 'V':'44', 'W':'34', 'X':'6', 'Y':'25', 'Z':'1',
-        'a': '26', 'b': '29', 'c': '56', 'd': '3', 'e': '24', 'f': '0', 'g': '47', 'h': '27', 'i': '22', 'j': '41', 'k': '16', 'm': '11', 'n': '37', 'o': '2',
-        'p': '35', 'q': '21', 'r': '17', 's': '33', 't': '30', 'u': '48', 'v': '23', 'w': '55', 'x': '32', 'y': '14','z':'19'
+        '1': '13', '2': '12', '3': '46', '4': '31', '5': '43', '6': '18', '7': '40', '8': '28', '9': '5',
+        'A': '54', 'B': '20', 'C': '15', 'D': '8', 'E': '39', 'F': '57', 'G': '45', 'H': '36', 'J': '38', 'K': '51',
+        'L': '42', 'M': '49', 'N': '52', 'P': '53', 'Q': '7', 'R': '4', 'S': '9', 'T': '50', 'U': '10', 'V': '44',
+        'W': '34', 'X': '6', 'Y': '25', 'Z': '1',
+        'a': '26', 'b': '29', 'c': '56', 'd': '3', 'e': '24', 'f': '0', 'g': '47', 'h': '27', 'i': '22', 'j': '41',
+        'k': '16', 'm': '11', 'n': '37', 'o': '2',
+        'p': '35', 'q': '21', 'r': '17', 's': '33', 't': '30', 'u': '48', 'v': '23', 'w': '55', 'x': '32', 'y': '14',
+        'z': '19'
 
     }
     # 2. 将key对应的value存入一个列表
@@ -55,17 +74,19 @@ def bv2av(Bv):
 
     return sum ^ temp
 
-async def get_top_comments(av:str):
-    av= str(av)
+
+async def get_top_comments(av: str):
+    av = str(av)
     if av[0:2] == "BV":
-        avcode= bv2av(av)
+        avcode = bv2av(av)
     else:
-        avcode = av.replace("av","")
+        avcode = av.replace("av", "")
     async with httpx.AsyncClient() as client:
         headers = {'Content-Type': "application/x-www-form-urlencoded"}
-        r = await client.get(url=f'https://api.bilibili.com/x/v2/reply/main?next=0&type=1&oid={avcode}', headers=headers)
-    rd =  json.loads(r.text)
-    if rd['code']=="0":
+        r = await client.get(url=f'https://api.bilibili.com/x/v2/reply/main?next=0&type=1&oid={avcode}',
+                             headers=headers)
+    rd = json.loads(r.text)
+    if rd['code'] == "0":
         if not rd["data"]:
             return None
     hot_comments = rd['data']['replies'][:3]
@@ -73,45 +94,61 @@ async def get_top_comments(av:str):
     for c in hot_comments:
         name = c['member']['uname']
         txt = c['content']['message']
-        msg +=f'{name}: {txt}\n\n'
+        msg += f'{name}: {txt}\n\n'
     return msg
 
 
+async def get_av_data(av_list: list[str]) -> list[str]:
+    msg_list = []
+    for avcode in av_list:
+        msg = ""
+        if avcode[0:2].upper() == "BV":
+            avcode = bv2av(avcode)
+        elif avcode[0:2].lower() == "av":
+            avcode = avcode.replace("av", "")
+        elif avcode[0:7].lower() == "b23.tv/":
+            if b_b23tv:
+                msg += avcode + ", "
+                avcode = bv2av(b23tv2bv(avcode))
+            else:
+                continue
+        else:
+            continue
+        new_url = url + f"?aid={avcode}"
+        async with httpx.AsyncClient() as client:
+            headers = {'Content-Type': "application/x-www-form-urlencoded"}
+            r = await client.get(new_url, headers=headers)
+        rd = json.loads(r.text)
+        if rd['code'] == 0:
+            if not rd["data"]:
+                continue
+        else:
+            continue
+        msg += avcode + ":" + "\n"
+        try:
+            title = rd['data']['title']
+            pic = rd['data']['pic']
+            stat = rd['data']['stat']
+            view = stat['view']
+            danmaku = stat['danmaku']
+            reply = stat['reply']
+            fav = stat['favorite']
+            coin = stat['coin']
+            share = stat['share']
+            like = stat['like']
+            link = f"https://www.bilibili.com/video/av{avcode}"
+            desc = rd['data']['desc']
 
+            msg += "标题:" + title + "\n" + MessageSegment.image(
+                pic) + f"播放:{view} 弹幕:{danmaku} 评论:{reply} 收藏:{fav} 硬币:{coin} 分享:{share} 点赞:{like} \n点击连接进入: \n{link}\n简介: {desc}"
 
-async def get_av_data(av):
-    av= str(av)
-    if av[0:2] == "BV":
-        avcode= bv2av(av)
-    else:
-        avcode = av.replace("av","")
-    new_url =  url + f"?aid={avcode}"
-    async with httpx.AsyncClient() as client:
-        headers = {'Content-Type': "application/x-www-form-urlencoded"}
-        r = await client.get(new_url, headers=headers)
-    rd=  json.loads(r.text)
-    if rd['code']=="0":
-        if not rd["data"]:
-            return None
-    try:
-        title = rd['data']['title']
-        pic = rd['data']['pic']
-        stat = rd['data']['stat']
-        view = stat['view']
-        danmaku = stat['danmaku']
-        reply = stat['reply']
-        fav = stat['favorite']
-        coin = stat['coin']
-        share = stat['share']
-        like = stat['like']
-        link = f"https://www.bilibili.com/video/av{avcode}"
-        desc = rd['data']['desc']
+            print(msg)
+            if b_comments:
+                msg += await get_top_comments(avcode)
 
-        msg =  "标题:" + title + "\n" + MessageSegment.image(pic) + f"播放:{view} 弹幕:{danmaku} 评论:{reply} 收藏:{fav} 硬币:{coin} 分享:{share} 点赞:{like} \n点击连接进入: \n{link}\n简介: {desc}"
+            msg_list.append(msg)
+        except:
+            msg += "错误!!! 没有此av或BV号。"
 
-        print(msg)
-        if b_comments:
-            msg+=await get_top_comments(avcode)
-        return msg
-    except:
-        return "错误!!! 没有此av或BV号。"
+        msg_list.append(msg)
+    return msg_list
