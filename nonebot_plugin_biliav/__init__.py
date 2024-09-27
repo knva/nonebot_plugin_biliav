d8ac34aa0d1d38851b932b1e408fd8c427e825b7
knva <txt_ch@163.com>
2024-09-27 11:36:36 +0800

    Update NoneBot biliav小程序 查看插件 to version 1.0.12

--- nonebot_plugin_biliav/__init__.py
+++ nonebot_plugin_biliav/__init__.py
@@ -2,10 +2,10 @@
 import asyncio
 
 from nonebot import get_driver, on_regex
-from .config import Config
 from nonebot.typing import T_State
 from nonebot.adapters.onebot.v11 import Bot, Event
 from nonebot.params import T_State
+from nonebot.plugin import PluginMetadata
 
 global_config = get_driver().config
 config = global_config.dict()
@@ -15,6 +15,14 @@ b_sleep_time = int(b_sleep_time)
 from .data_source import get_av_data
 import re
 
+__plugin_meta__ = PluginMetadata(
+    name="NoneBot biliav小程序 查看插件",
+    description="NoneBot biliav小程序 查看插件",
+    usage="发送av170001或者BV号即可查看。",
+    type="application",
+    extra={},
+)
+
 # Export something for other plugin
 # export = nonebot.export()
 # export.foo = "bar"
@@ -22,13 +30,13 @@ import re
 # @export.xxx
 # def some_function():
 #     pass
-biliav = on_regex("[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}")
+biliav = on_regex(r"[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}")
 
 
 @biliav.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
     avcode_list: list[str] = re.compile(
-        "[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}").findall(
+        r"[Aa][Vv]\d{1,12}|[Bb][Vv]1[A-Za-z0-9]{2}4.1.7[A-Za-z0-9]{2}|[Bb]23\.[Tt][Vv]/[A-Za-z0-9]{7}").findall(
         str(event.get_message()))
     if not avcode_list:
         return
